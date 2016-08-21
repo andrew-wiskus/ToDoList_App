b3c9c81f61ba4a3de75fd02d5c5467bf24c51bc1
Drew Wiskus <andrewwiskus@gmail.com>
2016-08-21 17:38:14 -0500

    added base functionality

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -3,10 +3,18 @@ $(document).ready(function() {
 
     //hiddenElement
     $('#addTask_form').hide();
-    //hideComments();
+
 
 
     //listeners
+    //---------------------------------------------------------------
+    //---------------add task functionality listeners----------------
+    //---------------------------------------------------------------
+    $('#addTaskButton').on("click", addTaskClick);
+    $('#cancel').on("click", function() {
+        //TODO: fix this..? shitty way to do this.
+        location.reload();
+    });
     $('#addTask').on("click", function() {
         $('#addTask').hide();
         $('#addTaskLabel').hide();
@@ -18,40 +26,127 @@ $(document).ready(function() {
         $('#addTaskLabel').hide();
         $('#addTask_form').show();
     });
+    //------------------------------------------------------------
+    //--------------task item icon listeners----------------------
+    //------------------------------------------------------------
+    $('#task-list').on("click", ".showDescription", commentIconClick);
+    $('#task-list').on("click", ".checkBox", function() {
+        $(this).toggleClass('glyphicon-unchecked').toggleClass('glyphicon-check');
+        $(this).parent().children().first().next().toggleClass('taskComplete');
 
-    $('#addTaskButton').on("click", function() {
-        event.preventDefault();
-        var taskData = {};
 
-        $.each($('#addTask_form').serializeArray(), function(i, field) {
-            taskData[field.name] = field.value;
-        });
-        console.log("typedData:", taskData);
+        var statusID = $(this).parent().attr('id');
+        var taskStatus = {};
+
+        if ($(this).parent().children().first().next().hasClass('taskComplete')) {
+            taskStatus.status = true;
+        } else {
+            taskStatus.status = false;
+        }
 
         $.ajax({
-            type: 'POST',
-            url: '/todoList',
-            data: taskData,
-            success: function() {
-                console.log('/POST success function ran');
-                //empty and repopulate #dataTable
-                $('#task-list').empty();
-                location.reload();
+            type: 'PUT',
+            url: '/todoList/' + statusID,
+            data: taskStatus,
+            success: function() {},
+            error: function() {
 
+            }
+        });
+    });
+
+    //---------------------------------------------------------------
+    //--------------edit task functionality--------------------------
+    //---------------------------------------------------------------
+    $('#task-list').on("click", ".edit_icon", editIconClick);
+    $('#task-list').on("click", ".edit_Delete", function() {
+        var rowClicked = $(this).parent().parent().attr("id");
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/todoList/' + rowClicked,
+            success: function() {
+                console.log('DELETED ITEM: ID:', rowClicked);
+
+                $('#task-list').empty();
+                loadTaskData();
             },
             error: function() {
-                console.log('/POST didnt work');
+                console.log("error in delete");
             }
-
         });
-
     });
 
-    $('#cancel').on("click", function() {
-        location.reload();
-    });
+
 
 });
+
+
+function addTaskClick() {
+    //TODO: stop reloading page to top/scrolling or w.e. seemless addition to page
+    event.preventDefault();
+    var taskData = {};
+
+    $.each($('#addTask_form').serializeArray(), function(i, field) {
+        taskData[field.name] = field.value;
+    });
+    console.log("typedData:", taskData);
+
+    $.ajax({
+        type: 'POST',
+        url: '/todoList',
+        data: taskData,
+        success: function() {
+            console.log('/POST success function ran');
+            //empty and repopulate #dataTable
+            $('#task-list').empty();
+            location.reload();
+
+        },
+        error: function() {
+            console.log('/POST didnt work');
+        }
+
+    });
+
+}
+
+function commentIconClick() {
+    var clickedRow = $(this).parent().attr("id");
+
+    if ($('#taskComment' + clickedRow).text() === "") {
+        console.log("blank");
+        //TODO: append comment input field
+
+    } else if ($('#taskComment' + clickedRow).is(":visible") === true) {
+        $('#taskComment' + clickedRow).hide();
+    } else {
+        $('#taskComment' + clickedRow).show();
+    }
+}
+
+function editIconClick() {
+    var clickedRow = ($(this).parent().attr('id'));
+    $(this).toggleClass("activeButton");
+    if ($('#editDisplay' + clickedRow).is(":visible") === true) {
+        $('#editDisplay' + clickedRow).hide();
+        $(this).text("");
+    } else {
+        $('#editDisplay' + clickedRow).show();
+        $(this).text("Cancel");
+        //TODO: FIX THIS CANCEL BUTTON SO UGLY
+    }
+
+    //TODO: make listeners for edit/delete/cancel
+    //TODO: make text input field for edit
+    //TODO: delete request for delete
+    //TODO: on cancel hide #editDisplay
+
+}
+
+function hideComments() {
+    $('.taskCommentText').hide();
+}
 
 function loadTaskData() {
     $.ajax({
@@ -63,25 +158,41 @@ function loadTaskData() {
             data.forEach(function(rowData, i) {
                 var taskDescription = "";
                 var hasDescription = "";
+                var isComplete = "";
+
                 if (rowData.task_description !== null) {
                     taskDescription = rowData.task_description;
                     hasDescription = "hasDescription";
                 }
 
+                if (rowData.task_completed === false) {
+                    isChecked = "glyphicon-unchecked";
+                    isComplete = "";
+                } else {
+                    isChecked = "glyphicon-check";
+                    isComplete = "taskComplete";
+                }
 
-                console.log(rowData);
+
+
                 var $el = $('<div id="' + rowData.id + '"class="task-list-item"></div>');
-                var appendString = '<span class="checkBox glyphicon glyphicon glyphicon-unchecked"></span>' +
-                    '<span class="task_name">' + rowData.task_name + '</span>' +
+                var appendString = '<span class="checkBox glyphicon glyphicon ' + isChecked + '"></span>' +
+                    '<span class="task_name ' + isComplete + '">' + rowData.task_name + '</span>' +
                     '<span class="showDescription ' + hasDescription + ' glyphicon glyphicon-comment"></span>' +
+                    '<span class="edit_icon glyphicon glyphicon-edit"></span>' +
                     '<span class="label_icon glyphicon glyphicon-asterisk"></span>' +
                     '<span class="task_label">' + rowData.task_label + '</span>' +
                     '<div id="taskComment' + rowData.id + '" class="taskComment"><p class="taskCommentText">' + taskDescription + '</p></div>' +
+                    '<div id="editDisplay' + rowData.id + '" class="editDisplay"><a class="edit_Delete" href="#">Delete</a>//<a class="edit_Content" href="#">Edit Content</a></div>' +
+                    '<div id="editComment' + rowData.id + '"></div>' +
                     '<div id="underlineTask"></div>';
 
                 $el.append(appendString);
 
                 $('#task-list').append($el);
+                $('.taskComment').hide();
+                $('.editDisplay').hide();
+
             });
         },
 
--- server/routes/todoList.js
+++ server/routes/todoList.js
@@ -4,6 +4,30 @@ var pg = require('pg');
 var connectionString = 'postgres://localhost:5432/omicron';
 
 
+router.delete('/:id', function(req, res) {
+    var id = req.params.id;
+    pg.connect(connectionString, function(err, client, done) {
+        if (err) {
+            res.sendStatus(500);
+            console.log("\n \n \n \n!!!HEY ERROR CONSOLE LOG HERE!!!\n error in DELTE, pg.connect\n", err, "\n \n \n \n");
+        }
+
+        //To manage strings and refrences cleaner
+        var refrenceValues = [id];
+        var queryString = 'DELETE FROM taskList WHERE id = $1';
+
+        client.query(queryString, refrenceValues,
+            function(err, result) {
+                done();
+                if (err) {
+                    console.log("\n \n \n \n!!!HEY ERROR CONSOLE LOG HERE!!!\n error in DELETE, client.query: ", err, "\n \n \n \n");
+                    res.sendStatus(500);
+                    return;
+                }
+                res.sendStatus(200);
+            });
+    });
+});
 
 
 router.get('/', function(req, res) {
@@ -29,7 +53,35 @@ router.get('/', function(req, res) {
     });
 });
 
+router.put('/:id', function(req, res) {
+    var id = req.params.id;
+    var taskStatus = req.body.status;
+    pg.connect(connectionString, function(err, client, done) {
+        if (err) {
+            console.log("\n \n \n \n!!!HEY ERROR CONSOLE LOG HERE!!!\n error in PUT, pg.connect", err, "\n \n \n \n");
+            res.sendStatus(500);
+        }
 
+        //To manage strings and refrences cleaner
+        var queryString = 'UPDATE taskList SET task_completed = $1 WHERE id = $2';
+        var refrenceValues = [taskStatus, id];
+
+        client.query(queryString, refrenceValues,
+
+            function(err, result) {
+                done();
+                if (err) {
+                    res.sendStatus(500);
+                    console.log("\n \n \n \n!!!HEY ERROR CONSOLE LOG HERE!!!\n error in PUT, client.query: ", err, "\n \n \n \n");
+                    return;
+                }
+                res.sendStatus(200);
+
+            });
+
+    });
+
+});
 router.post('/', function(req, res) {
     var item = req.body;
 
@@ -40,8 +92,8 @@ router.post('/', function(req, res) {
         }
 
         //To manage strings and refrences cleaner
-        var queryString = 'INSERT INTO taskList (task_name, task_label) VALUES ($1, $2)';
-        var refrenceValues = [item.task_name, item.task_label];
+        var queryString = 'INSERT INTO taskList (task_name, task_label, task_priority, task_completed) VALUES ($1, $2, $3, $4)';
+        var refrenceValues = [item.task_name, item.task_label, item.task_priority, false];
 
 
         client.query(queryString, refrenceValues,
